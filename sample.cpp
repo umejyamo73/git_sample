aa16fbd89f1c23e365a3de2c2bdaca3fb0a0cc9e
arare <ararecs252@gmail.com>
2018-11-13 20:36:21 +0900

    add multiple

--- sample.cpp
+++ sample.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 
 int sum(int,int);
+int multiple(int,int);
 
 int main(){
     std::cout << "This is master of the world" << std::endl;
@@ -11,4 +12,8 @@ int main(){
 
 int sum(int a,int b){
     return a+b;
+}
+
+int multiple(int a,int b){
+    return a*b;
 }
